ec8abddf361296f14a4e0d8e570f48921578dd15
Eddy Harrington <hisfastness@outlook.com>
2020-05-21 12:12:32 -0700

    remove session temp file dir

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def after_request(response):
 
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
